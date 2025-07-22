d6a0b45d8ab0694adab4c85c3da5b990bd937602
LAPTOP-HHTFEQI1\RNYUMUL <randel.angelo10@gmail.com>
2025-07-22 18:22:40 +0800

    index, mendoza, yumul, and css adjusted, assets added

--- script.js
+++ script.js
@@ -111,3 +111,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementById("myAudio");
+  audio.volume = 0.2;
+});
+
